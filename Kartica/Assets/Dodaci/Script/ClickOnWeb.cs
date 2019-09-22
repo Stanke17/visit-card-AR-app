0a68966d741cacc8c8367526f8c0bedaf5e79225
Stanke17 <Salas.ss94@gmail.com>
2019-09-22 21:35:05 +0200

    FirstProjectSubmit

---
+++ Kartica/Assets/Dodaci/Script/ClickOnWeb.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(MeshRenderer))]
+public class ClickOnWeb : MonoBehaviour
+{
+    MeshRenderer mesh;
+
+    void Awake()
+    {
+        mesh = GetComponent<MeshRenderer>();
+    }
+
+    void OnMouseDown()
+    {
+        if(gameObject.name == "LinkedIn")
+        {
+            Application.OpenURL("https://www.linkedin.com/in/sa%C5%A1a-stankovi%C4%87-0b789192/");
+        }
+        else if(gameObject.name == "Website")
+        {
+            Application.OpenURL("https://remex.rs/");
+        }
+        else if(gameObject.name == "Email")
+        {
+            Application.OpenURL("mailto:sasa.stankovic@remex.rs");
+        }
+
+  
+    }
+
+    
+
+    
+}
---
+++ Kartica/Assets/Script/StartScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class StartScript : MonoBehaviour, ITrackableEventHandler
+{
+    public GameObject video;
+
+    private TrackableBehaviour mtrackableBehaviour;
+
+    void Start()
+    {
+        video.SetActive(false);
+        mtrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mtrackableBehaviour)
+        {
+            mtrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+           newStatus == TrackableBehaviour.Status.TRACKED ||
+           newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+
+            video.SetActive(true);
+
+        }
+        else
+        {
+            video.SetActive(false);
+        }
+    }
+}
